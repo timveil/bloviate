07098c14c00df472a2912fcbeab28d4a42c57a86
Tim Veil <3260845+timveil@users.noreply.github.com>
2020-03-03 21:44:18 -0500

    big decimal generator is pretty clean now

--- src/main/java/io/bloviate/gen/BigDecimalGenerator.java
+++ src/main/java/io/bloviate/gen/BigDecimalGenerator.java
@@ -16,14 +16,45 @@
 
 package io.bloviate.gen;
 
+import org.apache.commons.lang3.RandomStringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.math.BigDecimal;
+import java.util.StringJoiner;
 
 public class BigDecimalGenerator implements DataGenerator<BigDecimal> {
 
+    final Logger logger = LoggerFactory.getLogger(getClass());
+
+    private final Integer maxPrecision;
+    private final Integer maxDigits;
 
     @Override
     public BigDecimal generate() {
-        return BigDecimal.valueOf(new DoubleGenerator.Builder().build().generate());
+
+        if (maxPrecision != null) {
+
+            // maxPrecision can be enormous (131089 for CRDB) and not helpful for testing therefore we significantly reduce
+            int precision = Math.min(maxPrecision, 25);
+            int scale = (maxDigits != null ? maxDigits : 0);
+
+            StringJoiner joiner = new StringJoiner(".");
+            joiner.add(RandomStringUtils.randomNumeric(1, (precision - scale) + 1));
+
+            if (scale > 0) {
+                joiner.add(RandomStringUtils.randomNumeric(1, scale + 1));
+            }
+
+            String bigDecimalString = joiner.toString();
+
+            logger.debug("precision {}, scale {}, bd {}", precision, scale, bigDecimalString);
+
+            return new BigDecimal(bigDecimalString);
+        } else {
+            return BigDecimal.valueOf(new DoubleGenerator.Builder().build().generate());
+        }
+
     }
 
     @Override
@@ -33,12 +64,26 @@ public class BigDecimalGenerator implements DataGenerator<BigDecimal> {
 
     public static class Builder {
 
+        private Integer maxPrecision;
+        private Integer maxDigits;
+
+        public Builder precision(Integer maxPrecision) {
+            this.maxPrecision = maxPrecision;
+            return this;
+        }
+
+        public Builder digits(Integer maxDigits) {
+            this.maxDigits = maxDigits;
+            return this;
+        }
+
         public BigDecimalGenerator build() {
             return new BigDecimalGenerator(this);
         }
     }
 
     private BigDecimalGenerator(Builder builder) {
-
+        this.maxDigits = builder.maxDigits;
+        this.maxPrecision = builder.maxPrecision;
     }
 }
