4eea7d66c9796c236da58eb3e598d190e20b19e5
Tim Veil <3260845+timveil@users.noreply.github.com>
2021-12-22 19:18:18 -0500

    updating copyright headers

--- src/main/java/io/bloviate/file/ColumnDefinition.java
+++ src/main/java/io/bloviate/file/ColumnDefinition.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 Tim Veil
+ * Copyright (c) 2021 Tim Veil
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
