76dad95bf172d3edc2743b267bfb2c81027d27f8
Tim Veil <3260845+timveil@users.noreply.github.com>
2022-07-03 08:30:05 -0400

    working on validators

--- src/test/java/io/bloviate/db/PostgresFillerTest.java
+++ src/test/java/io/bloviate/db/PostgresFillerTest.java
@@ -37,14 +37,15 @@ class PostgresFillerTest extends BasePostgresTest {
     @Test
     void fillTPCCWithConfigs() throws SQLException {
 
-        DatabaseConfiguration configuration = new DatabaseConfiguration(128, 10, new PostgresSupport(), TPCCConfiguration.build(1));
+        DatabaseConfiguration configuration = new DatabaseConfiguration(128, 10, new PostgresSupport(), TPCCConfiguration.build(2));
         fillDatabase("create_tpcc.postgres.sql", configuration, new Validator() {
             @Override
             public void validate(Connection connection, DatabaseConfiguration configuration) throws SQLException {
                 try (Statement statement = connection.createStatement();
                      ResultSet rs = statement.executeQuery("select count(*) from item")) {
                     rs.next();
-                    Assertions.assertEquals(rs.getInt(1), configuration.tableConfiguration("item").orElseThrow().rowCount());
+                    int count = rs.getInt(1);
+                    Assertions.assertEquals(count, configuration.tableConfiguration("item").orElseThrow().rowCount());
                 }
 
                 try (Statement statement = connection.createStatement();
@@ -62,6 +63,38 @@ class PostgresFillerTest extends BasePostgresTest {
                     Assertions.assertTrue(minNameLength >= 14 , String.format("min i_name is less than %d", minNameLength));
                     Assertions.assertTrue(maxPrice <= 100.00d, String.format("max i_price is greater than %3.2f", maxPrice));
                     Assertions.assertTrue(minPrice >= 1.00d, String.format("min i_price is less than %1.2f", minPrice));
+                }
+
+                try (Statement statement = connection.createStatement();
+                     ResultSet rs = statement.executeQuery("select count(*) from warehouse")) {
+                    rs.next();
+                    int count = rs.getInt(1);
+                    Assertions.assertEquals(count, configuration.tableConfiguration("warehouse").orElseThrow().rowCount());
+                }
+
+                try (Statement statement = connection.createStatement();
+                     ResultSet rs = statement.executeQuery("select max(char_length(w_name)), min(character_length(w_name)), max(char_length(w_street_1)), min(character_length(w_street_1)), max(char_length(w_street_2)), min(character_length(w_street_2)), max(char_length(w_city)), min(character_length(w_city)), max(w_tax), min(w_tax) from warehouse")) {
+                    rs.next();
+                    int maxNameLength = rs.getInt(1);
+                    int minNameLength = rs.getInt(2);
+                    int maxStreet1Length = rs.getInt(3);
+                    int minStreet1Length = rs.getInt(4);
+                    int maxStreet2Length = rs.getInt(5);
+                    int minStreet2Length = rs.getInt(6);
+                    int maxCityLength = rs.getInt(7);
+                    int minCityLength = rs.getInt(8);
+                    double maxTax = rs.getDouble(9);
+                    double minTax = rs.getDouble(10);
+                    Assertions.assertTrue(maxNameLength <= 10 , String.format("max w_name is greater than %d", maxNameLength));
+                    Assertions.assertTrue(minNameLength >= 6 , String.format("min w_name is less than %d", minNameLength));
+                    Assertions.assertTrue(maxStreet1Length <= 20 , String.format("max w_street_1 is greater than %d", maxStreet1Length));
+                    Assertions.assertTrue(minStreet1Length >= 10 , String.format("min w_street_1 is less than %d", minStreet1Length));
+                    Assertions.assertTrue(maxStreet2Length <= 20 , String.format("max w_street_2 is greater than %d", maxStreet2Length));
+                    Assertions.assertTrue(minStreet2Length >= 10 , String.format("min w_street_2 is less than %d", minStreet2Length));
+                    Assertions.assertTrue(maxCityLength <= 20 , String.format("max w_city is greater than %d", maxCityLength));
+                    Assertions.assertTrue(minCityLength >= 10 , String.format("min w_city is less than %d", minCityLength));
+                    Assertions.assertTrue(maxTax <= 0.2000d, String.format("max i_price is greater than %.4f", maxTax));
+                    Assertions.assertTrue(minTax >= 0.0000d, String.format("min i_price is less than %.4f", minTax));
                 }
             }
         });
